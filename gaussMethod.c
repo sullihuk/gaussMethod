849e6f00d3958d7bd0f32a8e67d96e8429d8023c
sullihuk <nyxa@nextmail.ru>
2023-01-25 05:09:52 -0500

    Starting

---
+++ gaussMethod.c
@@ -0,0 +1,10 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <math.h>
+
+int main ()
+{
+	float rate[5][6] = { {0, 4, 6, 0, 5, 20},{-2, -2, 0, 4, 4, -10}, {5, -1, -1, 5, 5, 34}, {-3, -1, 1, 5, 2, -2} {2, 6, 5, -2, 0, 37}};
+	float x, y, z, p, q, free;
+	
+	
